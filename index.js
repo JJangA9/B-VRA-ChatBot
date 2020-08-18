40cc33fa9523902e839be297d4ee5f114040b329
KimDongWook <dream4gstar@gmail.com>
2020-08-18 15:56:12 +0900

    initial_nodejs + index.js

---
+++ index.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const app = express();
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+
+const apiRouter = express.Router();
+
+app.use(logger('dev', {}));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.use('/api', apiRouter);
+
+apiRouter.post('/sayHello', function(req, res) {
+  const responseBody = {
+    version: "2.0",
+    template: {
+      outputs: [
+        {
+          simpleText: {
+            text: "hello I'm Ryan"
+          }
+        }
+      ]
+    }
+  };
+
+  res.status(200).send(responseBody);
+});
+
+apiRouter.post('/showHello', function(req, res) {
+  console.log(req.body);
+
+  const responseBody = {
+    version: "2.0",
+    template: {
+      outputs: [
+        {
+          simpleImage: {
+            imageUrl: "https://t1.daumcdn.net/friends/prod/category/M001_friends_ryan2.jpg",
+            altText: "hello I'm Ryan"
+          }
+        }
+      ]
+    }
+  };
+
+  res.status(200).send(responseBody);
+});
+
+app.listen(3000, function() {
+  console.log('Example skill server listening on port 3000!');
+});
