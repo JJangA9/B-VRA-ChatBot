aa53ca9de293b194eb9b5e217caf504d75c6afbd
Jeong You Ji <dbwl150236@naver.com>
2020-08-20 16:03:27 +0900

    create add function

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 
 const CaverJS = require('caver-js');
 const { numberToHex } = require('caver-js/packages/caver-utils');
+const Caver = require('caver-js');
 const caver = new CaverJS('https://api.baobab.klaytn.net:8651/');
 
 const apiRouter = express.Router();
@@ -17,7 +18,9 @@ app.use(bodyParser.urlencoded({
 
 app.use('/api', apiRouter);
 
-
+function addNumber(num1, num2){
+  return num1 + num2;
+};
 
 apiRouter.post('/sayHello', function(req, res) {
   const responseBody = {
@@ -56,9 +59,29 @@ apiRouter.post('/showHello', function(req, res) {
   res.status(200).send(responseBody);
 });
 
+apiRouter.post('/showResult', function(req, res) {
+  console.log(req.body);
+  var result = addNumber(3, 3)
+
+  const responseBody = {
+    version: "2.0",
+    template: {
+      outputs: [
+        {
+          simpleText: {
+            text: result
+          }
+        }
+      ]
+    }
+  };
+
+  res.status(200).send(responseBody);
+});
+
 app.listen(3000, function() {
   console.log('Example skill server listening on port 3000!');
   caver.klay.getBlockNumber().then((height) => {
     console.log(height);
-});
+  });
 });
